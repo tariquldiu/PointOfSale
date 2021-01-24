443a4de14649049b0ffbcd90abc48a97231de501
tariqul.diu.cse@gmail.com <tariqul.diu.cse@gmail.com>
2021-01-24 23:45:47 +0600

    First Commit

---
+++ SignUp/AngularJS/login.js
@@ -0,0 +1,39 @@
+﻿/// <reference path="angular.js" />
+var loginApp = angular.module('loginApp', ['LocalStorageModule', 'AuthApp']);
+
+loginApp.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptorService');
+})
+
+//loginApp.run(['authService', function (authService) {
+//    authService.fillAuthData();
+//}])
+
+loginApp.controller('loginController', ['$scope', '$window', 'authService', function ($scope, $window, authService) {
+    $scope.init = function () {
+        $scope.isProcessing = false;
+        $scope.LoginBtnText = "Sign In";
+    }
+
+    $scope.init();
+
+    $scope.loginData = {
+        userName: "",
+        password:""
+    }
+
+    $scope.Login = function () {
+        $scope.isProcessing = true;
+        $scope.LoginBtnText = "Signing in.....";
+
+        authService.login($scope.loginData).then(function (response) {
+            alert("Login Successfully");
+            $window.location.href = "/Home/Index";
+        }, function (error) {
+            alert(error.error_description);
+            $window.location.href = "/Users/Login";
+            $scope.init();
+        })
+    }
+
+}])
---
+++ SignUp/Models/Supplier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SignUp.Models
+{
+    [Table("Suppliers")]
+    public class Supplier
+    {
+        [Key]
+        public int SupplierId { get; set; }
+        public string SupplierName { get; set; }
+        public string ContactNo { get; set; } 
+        public string Company { get; set; }
+        public string CompanyAddress { get; set; }
+        public string FactoryAddress { get; set; }
+        public bool Status { get; set; }
+    }
+}
