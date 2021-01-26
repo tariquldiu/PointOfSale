f568a7f55d2834be3a71b9e2e1e0514beb9c3fac
mokabbir3485 <shuvo3485@gmail.com>
2021-01-26 23:21:20 +0600

    Fix Circular Depandence

---
+++ Models/Models/IdentityModels.cs
@@ -0,0 +1,50 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace SignUp.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("PointOfSaleContext", throwIfV1Schema: false)
+        {
+        }
+        
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<BankDetail> BankDetails { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<PurchesReturn> PurchesReturns { get; set; }
+        public DbSet<SalesReturn> SalesReturns { get; set; }
+        public DbSet<Stockin> Stockins { get; set; }
+        public DbSet<Summary> Summarys { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<User> users { get; set; }
+        public DbSet<WastageExchange> WastageExchanges { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+
+    }
+}
