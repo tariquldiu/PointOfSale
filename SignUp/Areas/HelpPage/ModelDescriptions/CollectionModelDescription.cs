de40039a0766bfcc8c114d66a0d765b962928c6c
mokabbir3485 <47471993+mokabbir3485@users.noreply.github.com>
2021-02-16 00:50:00 +0600

    Delete SignUp directory

--- SignUp/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace SignUp.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
